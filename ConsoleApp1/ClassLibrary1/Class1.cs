425d16f7b7f737a629f88160d3f5c99f4028a45a
Vasilisa1234 <81293134+Vasilisa1234@users.noreply.github.com>
2022-09-16 12:49:23 +0300

    Add files via upload

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Runtime.InteropServices;
+
+namespace ClassLibrary1
+{
+    public class Matrix
+    {
+        private int n;
+        private int[,] matr;
+        public Matrix () { }
+        public int N
+        {
+            get { return n; }
+            set { if (value > 0) n = value; }
+        }
+
+        // https://tumovsky.by/notes/csharp-class-matrix
+
+        public Matrix (int n)
+        {
+            this.n = n;
+            matr = new int[this.n, this.n];
+        }
+
+        public int this [int i, int j]
+        {
+            get
+            {
+                return matr[i, j];
+            }
+            set
+            {
+                matr[i, j] = value;
+            }
+        }
+
+        // Ввод матрицы с клавиатуры.
+        public void WriteMatr()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    matr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        // Вывод матрицы с клавиатуры.
+        public void ReadMatr()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Проверка матрицы А на единичность.
+        public void oneMatr(Matrix a)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] == 1 && i == j)
+                    {
+                        count++;
+                    }
+                }
+            }
+            if (count == a.N)
+            {
+                Console.WriteLine("Единичная");
+            }
+            else Console.WriteLine("Не единичная");
+        }
+
+        // Умножение матрицы А на число.
+        public static Matrix umnCh(Matrix a, int ch)
+        {
+            Matrix resMatr = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.N; j++)
+                {
+                    resMatr[i, j] = a[i, j] * ch;
+                }
+            }
+            return resMatr;
+        }
+
+        // Умножение матрицы А на матрицу Б.
+        public static Matrix umn(Matrix a, Matrix b)
+        {
+            Matrix resMatr = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+                for (int j = 0; j < b.N; j++)
+                    for (int k = 0; k < b.N; k++)
+                        resMatr[i, j] += a[i, k] * b[k, j];
+            return resMatr;
+        }
+
+        // перегрузка оператора умножения.
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.umn(a, b);
+        }
+
+        public static Matrix operator *(Matrix a, int b)
+        {
+            return Matrix.umnCh(a, b);
+        }
+
+        // Метод вычитания матрицы Б из матрицы А.
+        public static Matrix razn(Matrix a, Matrix b)
+        {
+            Matrix resMatr = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.N; j++)
+                {
+                    resMatr[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return resMatr;
+        }
+
+        // Перегрузка оператора вычитания.
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.razn(a, b);
+        }
+
+        // Сложение матриц А и Б.
+        public static Matrix Sum(Matrix a, Matrix b)
+        {
+            Matrix resMatr = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.N; j++)
+                {
+                    resMatr[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return resMatr;
+        }
+
+        // Перегрузка сложения матриц.
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.Sum(a, b);
+        }
+
+        // Деструктор Matrix.
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие:\n" +
+                "1. Умножение матрицы на число\n" +
+                "2. Сложение матриц\n" +
+                "3. Вычитание матриц\n" +
+                "4. Умножение матриц\n" +
+                "5. Возведение в степень матрицы\n" +
+                "6. Транспонирование матрицы\n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
